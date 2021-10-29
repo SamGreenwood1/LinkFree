6a73b1e88ceeac5e61d69725a77122548f567070
Sam Greenwood <jammer2001@gmail.com>
2021-10-28 20:26:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,3 @@ $( document ).ready(function() {
 $( ".link" ).click(function() {
     // Add here commands to run on link click
 });
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
